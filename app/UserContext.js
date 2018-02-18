f2c8585195cf958bd446f000a1898704e4869484
danieltoo <torresestradadaniel@gmail.com>
2018-02-17 23:22:23 -0600

    Cambié algoritmo de detección de campus

--- app/UserContext.js
+++ app/UserContext.js
@@ -4,53 +4,50 @@ import Functions from './functions/Functions'
 import Actions from './redux/actions/Actions'
 import store from './redux/reducers/index'
 
-import OCBConnection from "./services/OCBConnection"
-import DBase from './functions/DBase'
-
-
 class UserContext {
 
-   async watchContext () {
-        
-        let t =  this 
-        this.location= ""
-        t.campus = null
+    constructor () {
+        this.campus = null
         Actions.outCampus();
-        navigator.geolocation.watchPosition((position) =>{  
-        
-            AsyncStorage.getItem('campuslist').then((campuslist) =>{
+    }
 
-                let list = JSON.parse(JSON.parse("[" + campuslist + "]"))
-                let campus = null
-                list.map((camp) => {
-                    if (Functions.PointOnCampus([19.033347772359097 ,-98.31635484566372],camp.location)){ // Apatzingan    
-                     campus = camp  
-                    } 
-                })
-                if(campus !== null && t.campus !== campus){
-                    t.campus = campus
-                    Actions.inCampus(campus);
-                }else{
-                    Actions.outCampus();
-                }
-                
-            })
-            let location = `${position.coords.latitude} ,${position.coords.longitude}`
-            t.location = location
-            
+   async watchContext () {
+        let t =  this 
+        navigator.geolocation.watchPosition((position) =>{  
+            location = [19.033347772359097 ,-98.31635484566372]
+            if(t.campus === null) {
+                t.searchCampus(location)
+            } else {
+                if (!Functions.PointOnCampus(location,t.campus.location)){  
+                    t.searchCampus(location)
+                } 
+            }
         },
         (error) => {
             console.log(error.message)
         },
         { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter:0.5 },
         );
-
-
     }
-       
 
-    
-
+    searchCampus (location) {
+        let t =  this 
+        AsyncStorage.getItem('campuslist').then((campuslist) =>{
+            let list = JSON.parse(JSON.parse("[" + campuslist + "]"))
+            let campus = null
+            list.map((camp) => {
+                if (Functions.PointOnCampus(location,camp.location)){    
+                    campus = camp  
+                } 
+            })
+            if(campus !== null && t.campus !== campus){
+                t.campus = campus
+                Actions.inCampus(campus);
+            }else{
+                Actions.outCampus();
+            }
+        })
+    }
 }
 
 module.exports = new UserContext()
