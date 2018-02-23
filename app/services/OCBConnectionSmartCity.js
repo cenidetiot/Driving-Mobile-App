1dd9b81ff85fde08c918154dd6b4d508d0bf548b
danieltoo <torresestradadaniel@gmail.com>
2018-02-22 19:27:13 -0600

    Agregué archivo para conectar a smartcity

---
+++ app/services/OCBConnectionSmartCity.js
@@ -0,0 +1,39 @@
+
+import {AsyncStorage,ToastAndroid} from 'react-native'
+import OCB from 'ocb-sender';
+import NGSI from 'ngsi-parser'
+import NgsiModule from '../../NativeModules/NgsiModule';
+import config from '../../config/OCBconfig'
+
+class OCBConnection {
+
+	constructor () {
+		OCB.config(`http://${config.smartCityContext}`,config.port,config.version)
+	}
+
+	create (entity, message = false) {
+		
+	    let newJson = NGSI.parseEntity(entity)
+		OCB.createEntity(newJson)
+		.then((result) =>{
+			if(message != false)
+				ToastAndroid.showWithGravity( message, ToastAndroid.SHORT, ToastAndroid.CENTER);
+		})
+		.catch((err) => {
+			//ToastAndroid.showWithGravity( JSON.stringify(err) , ToastAndroid.SHORT, ToastAndroid.CENTER);
+		})
+		return newJson
+	}
+
+	update(entity, attr) {
+		var attribute = NGSI.parseAttrs(attr)
+		// Send to ContextBroker 
+		OCB.updateEntityAttrs(entity, attribute)
+		.then((result) => console.log(result))
+		.catch((err) => console.log(err))
+	}
+
+	
+}
+
+module.exports = new OCBConnection()
--- config/OCBconfig.js
+++ config/OCBconfig.js
@@ -3,5 +3,6 @@ export default {
 	//ip : '10.0.0.7',
 	ip : '130.206.113.226',
 	port: 1026,
-	version : 'v2'
+	version : 'v2',
+	smartCityContext : '207.249.127.132'
 }
